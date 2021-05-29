065bfbcda66b2da9a2d2fdfa444645f6d542850f
alpha-hexor <itsalexwilliams69@gmail.com>
2021-05-29 00:50:51 -0700

    third commit

--- javafix.sh
+++ javafix.sh
@@ -1,2 +1,14 @@
 sudo apt-get install openjdk-11-jdk
+clear
 sudo update-alternatives --config java
+clear
+echo -n "[-]Do you have zipalign(y/n): "; read opt1
+if [ $opt1 = "n" ]
+	then
+		sudo apt-get install zipalign
+fi
+clear
+
+echo "[*]It is recomended to delete /home/<username>/.local/share/apktool/framework/1.apk before executing the script"
+exit 1
+
